7e12f5f3c9d74cbd5b50654255567a71160e1cf0
InGodWeTrustt <fanat2016@mail.ru>
2023-07-03 00:21:55 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
 
@@ -13,27 +13,25 @@ def update_entry_value(entry, new_value=''):
     entry.insert(0, new_value)
 
 def fill_from_file():
-   path = filedialog.askopenfilename()
-   # Если файл не выбран или он не текстового формата, завершить выполнение функции
-   if path == '' or not path.endswith('.txt'): return 
-
-   with open(path, 'r', encoding='utf-8') as f:
-      for line in f:
-         name, value = line.strip().split(' ')
-         if name == 'Вес':
-            update_entry_value(weight_entry, value)
-         else:
-             update_entry_value(height_entry, value)
+    path = filedialog.askopenfilename()
+    # Если файл не выбран или он не текстового формата, завершить выполнение функции
+    if path == '' or not path.endswith('.txt'): return 
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            name, value = line.strip().split(' ')
+            if name == 'Вес':
+                update_entry_value(weight_entry, value)
+            else:
+                update_entry_value(height_entry, value)
                  
 def calc_bmi():
     try:
         kg = float(weight_entry.get())
         m = int(height_entry.get()) / 100
         if kg <=0 or m <= 0:
-           raise ValueError('Вес и рост должны быть больше нуля')
+            raise ValueError('Вес и рост должны быть больше нуля')
         bmi = kg/(m*m)
         bmi = round(bmi, 1)
-        
         if bmi < 18.5:
             messagebox.showinfo('Итог', f'ИМТ = {bmi} соответствует недостаточному весу')
         elif (bmi > 18.5) and (bmi < 24.9):
@@ -44,26 +42,32 @@ def calc_bmi():
             messagebox.showinfo('Итог', f'ИМТ = {bmi} соответствует ожирению') 
     except ValueError as e:
             messagebox.showerror('Ошибка', str(e))
-
-root = Tk()
+        
+root = tk.Tk()
 root.geometry('500x500') # ширина и высота в пикселях
 root.title('Расчет ИМТ') # название окна
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-frame = ttk.Frame(root, borderwidth=5)
+frame = tk.Frame(root)
 frame.grid(column=0, row=0)
 
-height_lbl = ttk.Label(frame, text='Введите ваш рост в см',font=('Arial Bold', 10)).grid(row=3, column=2)
-height_entry = ttk.Entry(frame)
+height_lbl = tk.Label(frame, text='Введите ваш рост в см',font=('Arial Bold', 10)).grid(row=3, column=2)
+height_entry = tk.Entry(frame)
 height_entry.grid(row=3, column=3, columnspan=2)
 
-weight_lbl = ttk.Label(frame, text='Введите ваш вес в кг',font=('Arial Bold', 10)).grid(row=4, column=2)
-weight_entry = ttk.Entry(frame)
+weight_lbl = tk.Label(frame, text='Введите ваш вес в кг',font=('Arial Bold', 10)).grid(row=4, column=2)
+weight_entry = tk.Entry(frame)
 weight_entry.grid(row=4, column=3, columnspan=2)
 
-calc_btn = ttk.Button(frame,text='Рассчитать ИМТ',command=calc_bmi).grid(row=5, column=4)
-clearfields_btn = ttk.Button(frame, text="Очистить поля",command=clear_fields).grid(row=5, column=3)
-opendialog_btn = ttk.Button(frame, text="Заполнить из файла",command=fill_from_file).grid(row=5, column=2)
+calc_btn = tk.Button(frame,text='Рассчитать ИМТ',command=calc_bmi)
+calc_btn.grid(row=5, column=4)
+clearfields_btn = tk.Button(frame, text="Очистить поля",command=clear_fields).grid(row=5, column=3)
+opendialog_btn = tk.Button(frame, text="Заполнить из файла",command=fill_from_file).grid(row=5, column=2)
+
+original_color_btn = calc_btn.cget('bg')
+
+calc_btn.bind('<Enter>', lambda x : calc_btn.config(bg="red"))
+calc_btn.bind('<Leave>', lambda x : calc_btn.config(bg=original_color_btn))
 
 root.mainloop()
